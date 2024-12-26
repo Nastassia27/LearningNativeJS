c4580daff866f623f735cdfa13fe009f2a809a38
Nastassia27 <anastasia.derugo@gmail.com>
2024-12-26 19:49:07 +0400

    destructing arrays

--- src/07/07.tsx
+++ src/07/07.tsx
@@ -8,8 +8,21 @@ type PropsType = {
 title: string,
     man: ManType
 }
-export const Man: React.FC<PropsType>=(props)=>{//export const Man: React.FC<PropsType>=({title, man})
+export const Man: React.FC<PropsType>=(props)=>{
+    //export const Man: React.FC<PropsType>=({title, man})
+    //({title, man...props})
+    //({title, man...restProps})
     const {title, man}=props
 return <div></div>
 
 }
+
+// массивы
+/*let props ={
+    name: 'foo',
+    age: 12,
+    lessons:[{title:'hi'}, {title:'hi'}],
+}*/
+//const [l1,l2]=props.lessons
+//const [ ,l2]=props.lessons const [,,l2]=props.lessons const [l1,...restLessons]=props.lessons
+const[message, setMessage] = React.useState<string>('') //деструктуризация
